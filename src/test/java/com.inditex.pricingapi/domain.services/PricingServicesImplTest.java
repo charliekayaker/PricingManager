4fce772f3d0a634b8c902b9457185e766d8bd838
charlie <carlosalfonso.it@gmail.com>
2024-03-25 00:03:45 +0100

    use cases all defined

--- src/test/java/com.inditex.pricingapi/domain.services/PricingServicesImplTest.java
+++ src/test/java/com.inditex.pricingapi/domain.services/PricingServicesImplTest.java
@@ -1,6 +1,16 @@
 package com.inditex.pricingapi.domain.services;
 
 class PricingServicesImplTest {
+    private static PriceRepository mockedPriceRepository;
+    private static PriceServiceImpl service;
     private Price priceItem1;
     private Price priceItem2;
+
+    testGetByProductAndBrand_whenIsSuccess_shouldReturnPriceItem();
+
+    casesOfSuccessByAppliedDate();
+
+    testGetByProductAndBrand_whenPriceNotExists_shouldReturnNotFoundApiException();
+
+    testGetByProductAndBrand_whenProviderFails_shouldReturnApiException();
 }
